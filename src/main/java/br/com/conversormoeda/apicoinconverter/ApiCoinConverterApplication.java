9bdc3371e657e476c86e8b7834ed0a63cac20f7f
Marcio Costa <marcio.c8@gmail.com>
2021-06-09 21:30:08 -0300

    [API][TSK-01] - Initial commit of project

---
+++ src/main/java/br/com/conversormoeda/apicoinconverter/ApiCoinConverterApplication.java
@@ -0,0 +1,13 @@
+package br.com.conversormoeda.apicoinconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCoinConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCoinConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/conversormoeda/apicoinconverter/ApiCoinConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.conversormoeda.apicoinconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCoinConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
